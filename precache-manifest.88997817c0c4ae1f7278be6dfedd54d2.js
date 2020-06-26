edcade86532d8d58541a83f227608865c33d023f
misha2016 <mishabatsiashvili@yahoo.com>
2020-06-26 14:28:28 +0400

    Updates

---
+++ precache-manifest.88997817c0c4ae1f7278be6dfedd54d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d223ce56bcd2b65c3b87a88057bb7ab3",
+    "url": "/ReactWeatherApp/index.html"
+  },
+  {
+    "revision": "df3b4c83a496b5eeaa9e",
+    "url": "/ReactWeatherApp/static/css/main.bb62a197.chunk.css"
+  },
+  {
+    "revision": "e049a7dfa4014d98b265",
+    "url": "/ReactWeatherApp/static/js/2.306c28c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactWeatherApp/static/js/2.306c28c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3b4c83a496b5eeaa9e",
+    "url": "/ReactWeatherApp/static/js/main.783148c7.chunk.js"
+  },
+  {
+    "revision": "09a9479fb69ebe0c6774",
+    "url": "/ReactWeatherApp/static/js/runtime-main.fe0859bc.js"
+  },
+  {
+    "revision": "8060bbc7a09a3245d70104fba25b6368",
+    "url": "/ReactWeatherApp/static/media/loader.8060bbc7.gif"
+  }
+]);
